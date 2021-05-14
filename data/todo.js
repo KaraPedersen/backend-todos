e1629eb6f106eb101324aaf674c9c993c7677034
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-05-13 17:23:43 -0700

    setup and todos

---
+++ data/todo.js
@@ -0,0 +1,17 @@
+const todos = [
+  {
+    id: 1,
+    task: 'wash the dishes',
+    completed: false,
+    userId: 4,
+  },
+
+  {
+    id: 2,
+    task: 'do laundry',
+    completed: false,
+    userId: 4,
+  }
+];
+
+export default todos;
